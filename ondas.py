7cbdc90e298d276cd9394689ec00f2d43ee6a2ec
Gerardo02 <gerardo.jerez@hotmail.com>
2020-08-31 20:04:08 -0700

    second commit

--- ondas.py
+++ ondas.py
@@ -2,20 +2,29 @@ import sys
 
 sys.path.insert(1, 'dsp-modulo')
 
+
 from thinkdsp import SinSignal
 from thinkdsp import CosSignal
 from thinkdsp import decorate
 
 import matplotlib.pyplot as plt
 
-seno = SinSignal(freq=400, amp=1, offset=0)
-coseno = CosSignal(freq=750, amp=1, offset=0)
+seno = SinSignal(freq=5, amp=1, offset=0)
+coseno = CosSignal(freq=10, amp=1.3, offset=0)
+
+waveSeno = seno.make_wave(duration=1, start=0, framerate=11025)
+waveCoSeno = coseno.make_wave(duration=1, start=0, framerate=11025)
+
+waveResult = waveCoSeno + waveSeno
 
 decorate(xlabel="Tiempo (s)")
 decorate(ylabel="Amplitud")
 
 
-seno.plot()
-coseno.plot()
-
+waveSeno.plot()
+waveCoSeno.plot()
 plt.show()
+
+waveResult.plot()
+plt.show()
+
